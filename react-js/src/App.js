de536e6f6b14d7070e5c64fcf2c87c2c8cc3833d
sakshijainn <sakshijjain1997@gmail.com>
2021-01-02 01:23:34 +0530

    day1-react

---
+++ react-js/src/App.js
@@ -0,0 +1,17 @@
+import Displayname from "./Displayname";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     <h1><Displayname name="priya"/></h1>
+     <h1><Displayname name="vanshikas"/></h1>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-js/src/Displayname.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function Displayname(props) {
+    console.log(props);
+    return (
+        <div>
+            <h1>Hi Sakshi</h1>
+          
+        </div>
+    )
+}
+
+export default Displayname
+
+
+
+
+
+
+
+
+
+
+
+
+// // function DisplayName(props)
+// // {
+   
+// // }
+
+// // export default DisplayName;
+
+// // // or 
+// export function DisplayName(props)
+// {
+//     return <h1>Hi Simran</h1>
+// }
+
+// export const DisplayAgee =(props)=> {
+
+//     return <p>My age is 23</p>
+
+// }
